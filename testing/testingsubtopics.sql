2c867f84c41cd3f7c3d50ae461302b510c385605
Oleg <apxdono@gmail.com>
2013-05-28 18:59:08 +0300

    testing scripts

---
+++ testing/testingsubtopics.sql
@@ -0,0 +1,5 @@
+﻿select 3000 as EXPRECTED_TOTAL, t.total as ALL_RECORDS ,  (3000 - t.total) as MISSING, t2.total as RECORDS_WITH_TEXT ,t.total - t2.total as EMPTY_RECORDS, ROUND((CAST( t2.total as DECIMAL)/3000)*100) as success_rate , ROUND((CAST((3000 - t2.total) as DECIMAL)/3000)*100) as error_rate  from ( select count(*) as total from subtopics ) as t, ( select count(*) as total from subtopics WHERE display_name <> '' ) as t2
+
+
+--select * from subtopics
+--delete from subtopics
